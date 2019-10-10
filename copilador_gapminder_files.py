c4aa6783f5b3ebfafd44c7e66b0b01e778abadd7
dfrestrepr <55897770+dfrestrepr@users.noreply.github.com>
2019-10-09 20:03:08 -0500

    Create copilador_gapminder_files.py

---
+++ copilador_gapminder_files.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+PATH='/home/david/Descargas/Gapminder data/'
+
+files = os.listdir(PATH)
+
+data = pd.DataFrame()
+cont = 1
+for item in files:
+    if item.endswith('.csv'):
+        print('Archivo:', item, 'número:', cont)
+        temp = pd.read_csv(PATH+item)
+        temp = temp.melt(id_vars='country', var_name="Date",
+                value_name=item.split('csv')[0])
+        if data.empty:
+            data = temp
+        else:
+            data = pd.merge(data, temp, how='outer', on=['country', 'Date'])
+
+data = data.sort_values(['country', 'Date'])
+
+writer = pd.ExcelWriter(PATH+'copilado_gapminder.xlsx',  engine='xlsxwriter')
+data.to_excel(writer,sheet_name = 'gap', index=False)
+writer.save()
