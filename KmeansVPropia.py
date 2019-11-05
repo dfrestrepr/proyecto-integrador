e62e861371e71119c18c9e237fedcc7f87878658
Pablo Saldarriaga <psaldar2@eafit.edu.co>
2019-11-04 19:15:16 -0500

    Agrega al proceso la identifiacion y eliminaciond e outliers, ademas de elimimar variables redundantes

--- KmeansVPropia.py
+++ KmeansVPropia.py
@@ -4,10 +4,39 @@ Created on Sun Oct 13 10:38:34 2019
 
 @author: nicol
 """
-#### Version propia de kmeans para proyecto integrador
+#%%
+import os
+import funciones
 import numpy as np
 import pandas as pd
-import funciones
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+#%%
+from bokeh.plotting import figure
+from bokeh.io import output_file, save
+from bokeh.models import ColumnDataSource, HoverTool, BoxZoomTool, WheelZoomTool, ResetTool, PanTool
+#%%
+file_path = os.path.dirname(os.path.abspath(__file__))
+#%% 
+### Configuracion del logger
+import logging
+from logging.handlers import RotatingFileHandler
+
+file_name = 'Segmentacion_dinamica_'
+logger = logging.getLogger()
+dir_log = os.path.join(file_path, f'logs/{file_name}.log')
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+handler = RotatingFileHandler(dir_log, maxBytes=2000000, backupCount=10)
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s",
+                    handlers = [handler])
+#%%
+logger.info('*'*80)
+logger.info('Inicia ejecucion del programa')
+#### Version propia de kmeans para proyecto integrador
 
 ### Leemos los datos 
 datos = pd.read_csv('outputs/data_gapminder_proc.csv')
@@ -15,18 +44,19 @@ datos = pd.read_csv('outputs/data_gapminder_proc.csv')
 ### Por ahora, dejamos solo nombre pais y year, y dos variables explicativas
 datos = datos[datos.columns[[0,1,3,4,5,6]]]  
 
+
+datos = funciones.data_preprocessing(datos, alpha_outlier_detection =0.98, 
+                                     columns_not_numeric = {'country','Date'},
+                                     column_id = 'country')
+
 ### Estandarizo todos los datos
-from sklearn.preprocessing import StandardScaler
 datos_e = datos.copy()
 datos_e[datos_e.columns[2:]] = StandardScaler().fit_transform(datos_e[datos_e.columns[2:]])
 datos_e = datos_e.fillna(0)
 
 #### Aplicarle PCA a todos los datos
-from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 datos_pca = pca.fit_transform(datos_e[datos_e.columns[2:]])
-
-
 
 
 ### Fijar seed aleatoria
@@ -34,12 +64,13 @@ np.random.seed(1)
 
 ##### Inicio tomando los del primer year
 year_i = 2012   ### Year inicial a considerar
-X_data_df = datos_e[datos_e['Date']==year_i].reset_index(drop=True)
+filtro = datos_e['Date']==year_i
+X_data_df = datos_e[filtro].reset_index(drop=True)
 X_data = np.array(X_data_df[X_data_df.columns[2:]])
 
 
 ### Los que usare para el PCA seran
-X_data_pca = np.array(datos_pca[datos_e['Date']==year_i])
+X_data_pca = np.array(datos_pca[filtro])
 
 
 ## Numero de observaciones en cada periodo
@@ -61,11 +92,13 @@ centroids_pca = pca.transform(centroids)
 centroids_p = centroids.copy()
 
 ### Aplicar kmeans
-grados_pertenencia,etiquetas,centroids = funciones.kmeans(X_data,k,numiter,centroids,p_dista = p_dista)
+grados_pertenencia,etiquetas,centroids = funciones.kmeans(X_data,k,numiter,
+                                                          centroids,p_dista = p_dista)
 
 
 
-######### Variable global donde ire guardando la importancia de las variables en cada iteracion
+### Variable global donde ire guardando la importancia de las variables en cada 
+### iteracion
 imp_iters = []
 
 
@@ -98,10 +131,6 @@ imp_iters.append(imp_clus_prom)
 
 
 #PLotear con hover tool
-from bokeh.io import output_file, save
-from bokeh.models import ColumnDataSource, HoverTool, BoxZoomTool, WheelZoomTool, ResetTool, PanTool
-from bokeh.plotting import figure
-
 
 ### Colores a usar para cada cluster
 colores = ['blue', 'yellow', 'red', 'green', 'orange', 'purple']
@@ -171,8 +200,6 @@ for periodos in range(periodos_incluir):
     cambios_variables = X_data_viej - X_data    
     
 
-
-
     ################## Ponderacion dinamica
     ######## Pondero X_data
     X_data_ori = X_data.copy()  ### X data original (sin ponderacion)
@@ -200,8 +227,6 @@ for periodos in range(periodos_incluir):
         X_data_pond[:,peso] = X_data_pond[:,peso] * peso_variables[peso]
 
 
-
-
     ############ K means para los plots
     ### Etiquetas actuales de cada elemento para cada cluster
     etiquetas_prev = etiquetas.copy()
@@ -211,11 +236,6 @@ for periodos in range(periodos_incluir):
                                                               centroids,
                                                               p_dista = p_dista,
                                                               etiquetas = etiquetas)
-
-
-
-
-
 
 
 
@@ -252,12 +272,9 @@ for periodos in range(periodos_incluir):
 
 
 
-
-
-
-    
-    
-    ###################### Plotting
+    ###########################################################################
+    ################################ Plotting #################################
+    ########################################################################### 
     
     ### Consolido en listas las x, las y y las demas variables que vere para cada punto
     list_x = X_data_pca[:,0]
@@ -279,11 +296,21 @@ for periodos in range(periodos_incluir):
 
     
     ### Crear la figura
-    p = figure(plot_width=700, plot_height=500, tools=[hover, PanTool(), ResetTool(), BoxZoomTool(), WheelZoomTool()], title=str(year_i+1+periodos))
+    p = figure(plot_width=700, plot_height=500, tools=[hover, PanTool(), 
+                                                       ResetTool(), 
+                                                       BoxZoomTool(), 
+                                                       WheelZoomTool()],
+                                                 title=str(year_i+1+periodos))
     
     ### PLoteo cada conjunto
     for i in range(k):
-        source = ColumnDataSource(data={'x':list_x[np.where(etiquetas==i)], 'y':list_y[np.where(etiquetas==i)], 'xv':list_xv[np.where(etiquetas==i)], 'yv': list_yv[np.where(etiquetas==i)],'pais':list_pais[np.where(etiquetas==i)],'grados_p':grados_pertenencia[np.where(etiquetas==i)],'cluster_id':etiquetas[np.where(etiquetas==i)]})
+        source = ColumnDataSource(data={'x':list_x[np.where(etiquetas==i)], 
+                                        'y':list_y[np.where(etiquetas==i)], 
+                                        'xv':list_xv[np.where(etiquetas==i)], 
+                                        'yv': list_yv[np.where(etiquetas==i)],
+                                        'pais':list_pais[np.where(etiquetas==i)],
+                                        'grados_p':grados_pertenencia[np.where(etiquetas==i)],
+                                        'cluster_id':etiquetas[np.where(etiquetas==i)]})
         p.circle('x','y', size=12, 
                  fill_color=colores[i], source=source)
 
--- funciones.py
+++ funciones.py
@@ -76,7 +76,6 @@ def stepwise_logistic(X,Y,alpha =0.1):
         
     return variables_relevantes,model
 
-
 ### Se construye la funcion de variables relevantes utilizando metodos de
 ### arboles. Esta funcion entrena un arbol para el conjunto de datos y
 ### retorna las variables mas relevantes para explicar la variable Y. Hay
@@ -112,12 +111,69 @@ def variables_relevantes_arbol(X,Y,alpha = None):
     return importance
     return relevant_features
 
+### Esta funcion toma como entrada un dataframe, el nombre de las variables no
+### numericas, el nombre de la columna identificadora de los registros (ID del
+### cliente, pais, etc) y realiza una deteccion de outliers utilizando la 
+### distancia de Mahalanobis, adicional a esto, realiza la eliminacion de
+### variables redundantes en el conjunto de datos mirando la relacion lineal
+### de una variable contra todas las demas
+def data_preprocessing(datos, alpha_outlier_detection =0.95, columns_not_numeric = {}, 
+                       column_id = '', remove_vars = True, r_sqr_threshold = 0.9):
+    
+    ### Realiza la identificaciohn de outliers usando la distancia de 
+    ### Mahalanobis
+    logger.info('Realiza deteccion de outlier en el preprocesamiento de los datos')
+    outliers = outlier_detection_mahal(datos.drop(columns = columns_not_numeric),alpha = alpha_outlier_detection)
+    
+    ### En caso que no exista una columna identificadora para eliminar IDs
+    ### outliers, se eliminan todos los registros que se encontraron como
+    ### outlier    
+    if column_id == '':
+        logger.info('Realiza la eliminacion de outliers por registros encontrados')
+        datos = datos[outliers==0].reset_index(drop = True)    
+        
+    else:
+        logger.info('Realiza la eliminacion de outliers por IDs detectados')              
+        ids_outliers = datos[outliers==1][column_id].unique()
+        datos = datos[~datos[column_id].isin(ids_outliers)].reset_index(drop = True) 
+    
+    ### Esta seccion realiza eliminacion iterativa de variables de forma tal
+    ### que el R^2 de todas las variables sea menor a un umbral dado
+    if remove_vars:
+        logger.info('Se realiza el analisis del R^2 de todas las variables para eliminar variables redundantes')
+        to_drop = []
+        X_aux = datos.drop(columns = columns_not_numeric).copy()
+        while True:
+            var_names = list(X_aux.columns)
+            
+            inver = np.linalg.inv(X_aux.cov())
+    
+            ### Este es el R2 de cada variable con respecto a todas las demas, 
+            ### por eso seria un R2 por cada variable
+            r_squared = 1 - 1/(np.diagonal(X_aux.cov())*np.diagonal(inver))
+    
+            if any(r_squared > r_sqr_threshold):
+                
+                var_m = np.argmax(r_squared)
+                X_aux = X_aux.drop(columns = var_names[var_m])
+                
+                to_drop.append(var_names[var_m])
+                logger.info(f'Se elimina la variable:var_names[var_m] con el umbral: {r_sqr_threshold}')
+                
+            else:
+                break
+    
+    datos = datos.drop(columns = to_drop)
+    logger.info('Fin del preprocesamiento de los datos eliminando Outliers y variables redundantes')
+    return datos
+
+
 ### Esta funcion calcula la matriz de covarianza de Ledoit and Wolf, retorna
 ### la matriz de covarianza despues de aplicar el metodo de Shrinkage, ademas
 ### de retornar la media estimada. Esta funcion toma como parametros de entrada
 ### el conjunto de datos
 def LedoitWolf_covMatrix(X):
-    
+    logger.info('Se realiza el calculo de la matriz de covarianza con Shrinkage')
     cov = LedoitWolf().fit(X)
     cov_matrix = cov.covariance_
     mean_vector = cov.location_
@@ -125,18 +181,24 @@ def LedoitWolf_covMatrix(X):
     return cov_matrix, mean_vector
 
 
-### Funcion de distancia p
-def distancia(a,b, p):
-    ## if p = 0, que use la de mahalanobis (aun no esta programada)
+### Funcion de distancia p, tenga en cuenta que si p es cero, entonces se
+### asume que la distancia deseada es la de Mahalanobis
+def distancia(a,b, p, cov = np.array([])):
     
-    ### por ahora, la cuadratica
-    return np.linalg.norm(a-b, p)
+    if p == 0:
+        inv_cov = np.linalg.inv(cov)
+        dist = distance.mahalanobis(a, b, inv_cov)
+    else:
+        dist = np.linalg.norm(a-b, p) 
+    
+
+    return dist
 
 
 ### Inicializa los centroides para el metodo de kmeans, inicializa de forma
 ### aleatoria tantos centros como clusters
 def init_centroids(X_data,k):
-    
+    logger.info(f'Inicializacion de {k} centroides para el metodo de kmeans')
     centroids = []
     numdata = len(X_data)
     for i in range(k):
@@ -145,20 +207,29 @@ def init_centroids(X_data,k):
     return centroids
 
 ### Metodo de kmeans
-def kmeans(X_data,k,numiter,centroids,p_dista = 2,etiquetas = []):
-
+def kmeans(X_data,k,numiter,centroids,p_dista = 2,etiquetas = [], shrinkage = True):
+    logger.info('Inicializa el metodo de kmeans')
     numdata = len(X_data)    
     if len(etiquetas)==0:
+        logger.info('Se crean etiquetas ya que no fueron pasadas incialmente')
          ### Etiquetas actuales de cada elemento para cada cluster
         etiquetas = np.ones(numdata)*-1   ### Inicialmente, ningun elemento esta asignado
     
     ### Grados de pertenencia a cada cluster
     grados_pertenencia = []
-        
     
+    ### Se realiza el calculo de la matriz de varianzas y covarianzas en caso
+    ### de utilizar la distancia de Mahalanobis, ademas si se desea, se utiliza
+    ### una version bien condicionada de la matriz utilizando el metodo de
+    ### Shrinkage
+    if shrinkage:      
+        covariance_matrix, _ = LedoitWolf_covMatrix(X_data)
+    else:      
+        covariance_matrix = np.cov(X_data,rowvar=False)
+        
     ### Ahora empiezo las iteraciones
     for it in range(numiter):
-        
+        logger.info(f'Iteracion {it} de {numiter} para el metodo de kmeans')
         ### En cada iteracion, itero para todos los elementos
         for element in range(numdata):
             
@@ -167,7 +238,7 @@ def kmeans(X_data,k,numiter,centroids,p_dista = 2,etiquetas = []):
             ### le sumo 0.00001 a cada distancia para evitar division sobre cero
             distc = []
             for c in centroids:
-                distc.append(distancia(X_data[element], c, p_dista)+0.00001)
+                distc.append(distancia(X_data[element], c, p_dista,covariance_matrix)+0.00001)
             
 
             ### Encuentro el centroide al que tiene menor distancia
@@ -185,13 +256,6 @@ def kmeans(X_data,k,numiter,centroids,p_dista = 2,etiquetas = []):
             centroids[nearest_centroid] = np.mean(X_data[np.where(etiquetas==nearest_centroid)], axis=0)   
         
         centroids = np.array(centroids)
+    logger.info('Fin del algoritmo kmeans')
     return grados_pertenencia,etiquetas,centroids
-
-
-
-
-
-
-
-
 
